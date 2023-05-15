1cd5d19506be4ebd74f88afd3d84970050e24396
Fifaliana Gabriel <gabrielrazafimahatratra@gmail.com>
2023-05-15 11:15:24 +0300

    Premier commit via eclipse

---
+++ HelloWorld/src/com/gabrielrazafimahatratra/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.gabrielrazafimahatratra;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bonjour tout le monde, j'utilise eclipse");
+	}
+
+}
